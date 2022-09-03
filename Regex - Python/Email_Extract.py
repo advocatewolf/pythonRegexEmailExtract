21be9bad73ac1c5c57283bc664ee75aaed592d7b
advocatewolf <101878670+advocatewolf@users.noreply.github.com>
2022-09-03 22:46:22 +0800

    2022-09-03

--- Regex - Python/Email_Extract.py
+++ Regex - Python/Email_Extract.py
@@ -1,6 +1,10 @@
 import re
+import datetime
+
+
 f1 = open('Email_Extract_Unmodified.txt', 'r', encoding='utf8')
-f2 = open('Email_Extract_Modified.txt','w', encoding='utf8')
+    
+
 content = f1.read()
 res_content = re.sub(r'\r',"",content,flags=re.M)
 res_content = re.sub(r'Do you agree to the group rules from the admin\?\n.*\n(.*)',r'zxc\1',res_content,flags=re.M)
@@ -10,5 +14,6 @@ res_content = re.sub(r'\n\n',r'\n',res_content,flags=re.M)
 res_content = re.sub(r'^zxc',"",res_content,flags=re.M)
 res_content = re.sub(r'\n^(.*)@(.*)\n',r',\1@\2\n',res_content,flags=re.M)
 
-print(res_content)
-f2.write(res_content)
+print(str(datetime.date.today()))
+with open(str(datetime.date.today())+'-allieTask.txt','w', encoding='utf8') as f2:
+    f2.write(res_content)

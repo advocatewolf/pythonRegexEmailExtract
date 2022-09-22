fb6d59fd3261534c75502ee988d16ad39acaa628
advocatewolf <101878670+advocatewolf@users.noreply.github.com>
2022-09-22 09:38:01 +0800

    2022-09-22 update

--- Regex - Python/Email_Extract.py
+++ Regex - Python/Email_Extract.py
@@ -1,5 +1,6 @@
 import re
 import datetime
+import os
 
 
 f1 = open('Email_Extract_Unmodified.txt', 'r', encoding='utf8')
@@ -15,5 +16,15 @@ res_content = re.sub(r'^zxc',"",res_content,flags=re.M)
 res_content = re.sub(r'\n^(.*)@(.*)\n',r',\1@\2\n',res_content,flags=re.M)
 
 print(str(datetime.date.today()))
-with open(str(datetime.date.today())+'-allieTask.txt','w', encoding='utf8') as f2:
+i = 0
+
+path = str(datetime.date.today()) + '-allieTask-'+ str('%03d'%i) +'.txt'
+is_path = os.path.exists(path)
+while is_path is True:
+    i += 1
+    path = str(datetime.date.today()) + '-allieTask-'+ str('%03d'%i) +'.txt'
+    is_path = os.path.exists(path)
+    print(is_path)
+with open(str(datetime.date.today())+'-allieTask-'+str('%03d' % i)+'.txt','w', encoding='utf8') as f2:
     f2.write(res_content)
+        

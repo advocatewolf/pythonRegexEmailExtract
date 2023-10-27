8614c3a9f60eb919f088b274977e3fc9c035e8fb
advocatewolf <101878670+advocatewolf@users.noreply.github.com>
2023-10-28 01:52:33 +0800

    2023-10-28
    added v4 of email_extract.py

---
+++ allie_regex_python/Email_Extract_v4.py
@@ -0,0 +1,46 @@
+import re
+import datetime
+import os
+import pyperclip
+
+
+def process_content(content):
+    content = re.sub(r'\r', "", content, flags=re.M)
+    content = re.sub(r'Do you agree to the group rules from the admin\?\n.*\n(.*)', r'zxc\1', content, flags=re.M)
+    content = re.sub(r'(.*)@(.*)\n', r'zxc\1@\2\n', content, flags=re.M)
+    content = re.sub(r'^(?!zxc).*\n', "", content, flags=re.M)
+    content = re.sub(r'\n\n', r'\n', content, flags=re.M)
+    content = re.sub(r'^zxc', "", content, flags=re.M)
+    content = re.sub(r'\n^(.*)@(.*)\n', r',\1@\2\n', content, flags=re.M)
+    content = re.sub(r'^I agree', r'', content, flags=re.M)
+    content = re.sub(r'\.con$', r'\.com', content, flags=re.M)
+    return content
+
+
+def save_processed_content(content):
+    i = 0
+    number_of_lines = content.count("\n")
+    number_of_email = content.count('@')
+    text_to_copy = f"Allie Task -{number_of_email}-email - {number_of_lines}"
+    pyperclip.copy(text_to_copy)
+    file_path = f"{datetime.date.today()}-allieTask-{number_of_email:03d}email-{number_of_lines:03d}-members-{i:03d}.txt"
+
+    while os.path.exists(file_path):
+        i += 1
+        file_path = f"{datetime.date.today()}-allieTask-{number_of_email:03d}email-{number_of_lines:03d}-members-{i:03d}.txt"
+
+    with open(file_path, "w", encoding='utf-8') as f:
+        f.write(content)
+
+
+
+def main():
+    with open('Email_Extract_Unmodified.txt', 'r', encoding='utf-8') as f1:
+        content = f1.read()
+
+    processed_content = process_content(content)
+    save_processed_content(processed_content)
+
+
+if __name__ == "__main__":
+    main()
